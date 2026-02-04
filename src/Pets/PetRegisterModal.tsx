8f6343e2445eac63e8fec7293f0097763375a3a7
ynjw3758 <yoonjw7894@naver.com>
2026-02-04 17:32:08 +0900

    등록 ui 작업 중

--- src/Pets/PetRegisterModal.tsx
+++ src/Pets/PetRegisterModal.tsx
@@ -1,7 +1,20 @@
 import "./PetRegisterModal.scss";
 import beforeImg from"../assets/images/begorearrow.png";
+import CameraImg from "../assets/images/cameraimg.png";
+import { useState } from "react";
 
 const PetRegisterModal =() =>{
+    const[isprofile, setIsprofile]=useState<boolean>(false);
+    const[imgfile, setimgfile]=useState<string>("");
+
+    const handleChange =(event: React.ChangeEvent<HTMLInputElement>) =>{
+       const files = event.target.files;
+       if (!files || files.length === 0) return;
+       const file = files[0];
+       const imageUrl = URL.createObjectURL(file);
+       setimgfile(imageUrl);
+       setIsprofile(true);
+    }
 
     return(<div className="PetRegisterModal_Main">
         <div className="PetRegisterModal_Header">
@@ -9,9 +22,26 @@ const PetRegisterModal =() =>{
             <h3 className="PetRegisterModal_Title">새 애완동물 등록</h3>
         </div>
         <div className="PetRegisterModal_Contents">
-          <div className="PetRegister_ProfileCircle">
+            {!isprofile && (<>
+                      <label className="PetRegister_ProfileCircle">
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleChange}
+                    style={{ display: "none" }}
+                        />
              <span className="PetRegister_PlusIcon">+</span>
-          </div>
+                 <img
+                    src={CameraImg}
+                    alt="camera"
+                    className="PetRegister_CameraIcon"
+                    />
+          </label>
+            </>)}
+            {isprofile && (<div className="PetRegister_ProfileCircle">
+                <img src={imgfile} className="PetRegister_ProfileImg"/>
+            </div>)}
+            <p >사진을 업로드 해주세요</p>
         </div>
     </div>)
 

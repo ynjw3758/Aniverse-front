7a64d45fe2ef91ff40e0f23926b80cf3cddda327
jiwan <yunjiwan72@gmail.com>
2026-04-26 14:44:28 +0900

    렌더링 및 회워가입/로그인/아이디찾기 ui 리뉴얼 완성

--- src/Home/LandingFeatureSection.tsx
+++ src/Home/LandingFeatureSection.tsx
@@ -1,9 +1,12 @@
-import DogThumb from "../assets/images/landing_generated/pet_thumb_dog.png";
-import CatThumb from "../assets/images/landing_generated/pet_thumb_cat.png";
-import RabbitThumb from "../assets/images/landing_generated/pet_thumb_rabbit.png";
-import LizardThumb from "../assets/images/landing_generated/pet_thumb_lizard.png";
-import BirdThumb from "../assets/images/landing_generated/pet_thumb_bird.png";
-import FishThumb from "../assets/images/landing_generated/pet_thumb_fish.png";
+import CommunityFeedImage from "../assets/images/feature_cards/community_feed.svg";
+import PetProfileImage from "../assets/images/feature_cards/pet_profile.svg";
+import RecommendImage from "../assets/images/feature_cards/recommend.svg";
+import HospitalLocationImage from "../assets/images/feature_cards/hospital_location.svg";
+import EventsMeetupsImage from "../assets/images/feature_cards/events_meetups.svg";
+import MarketShareImage from "../assets/images/feature_cards/market_share.svg";
+import DailyNewsImage from "../assets/images/feature_cards/daily_news.svg";
+import SafetyReportImage from "../assets/images/feature_cards/safety_report.svg";
+import MultilingualImage from "../assets/images/feature_cards/multilingual.svg";
 
 type LandingFeatureSectionProps = {
   onPrimaryAction: () => void;
@@ -14,37 +17,55 @@ const featureCards = [
     title: "커뮤니티 & 피드",
     description: "일상 공유, 질문, 정보 교환 등 다양한 주제로 소통해요",
     tone: "pink",
-    images: [DogThumb, CatThumb],
+    image: CommunityFeedImage,
   },
   {
     title: "반려동물 정보 관리",
     description: "프로필, 건강, 기록을 한 곳에서 체계적으로 관리해요",
     tone: "green",
-    images: [RabbitThumb, LizardThumb],
+    image: PetProfileImage,
   },
   {
     title: "맞춤 추천",
-    description: "관심사와 정보 기반으로 맞춤 콘텐츠를 추천해드려요",
+    description: "관심사와 정보를 기반으로 맞춤 콘텐츠를 추천해드려요",
     tone: "purple",
-    images: [CatThumb, LizardThumb],
+    image: RecommendImage,
   },
   {
     title: "병원 & 위치 찾기",
     description: "내 주변 병원, 산책로, 펫 프렌들리 장소를 찾아요",
     tone: "blue",
-    images: [DogThumb, BirdThumb],
+    image: HospitalLocationImage,
   },
   {
     title: "이벤트 & 모임",
-    description: "다양한 이벤트와 모임에 참여하고 즐겨요",
+    description: "다양한 이벤트와 오프라인 모임에 참여하고 즐겨요",
     tone: "yellow",
-    images: [BirdThumb, RabbitThumb],
+    image: EventsMeetupsImage,
   },
   {
     title: "마켓 & 나눔",
-    description: "필요한 물품을 사고팔고 나눔으로 연결돼요",
+    description: "필요한 물품을 사고팔고 나눔으로 연결해요",
     tone: "orange",
-    images: [FishThumb, LizardThumb],
+    image: MarketShareImage,
+  },
+  {
+    title: "일상 & 소식",
+    description: "즐겨 찾는 주제에 맞춘 소식을 빠르게 확인해보세요",
+    tone: "green",
+    image: DailyNewsImage,
+  },
+  {
+    title: "안전 & 신고",
+    description: "긴급한 이슈나 신고 상황을 빠르게 기록하고 공유해요",
+    tone: "pink",
+    image: SafetyReportImage,
+  },
+  {
+    title: "다국어 지원",
+    description: "다양한 언어로 서비스를 이용하고 소통할 수 있어요",
+    tone: "blue",
+    image: MultilingualImage,
   },
 ];
 
@@ -58,9 +79,9 @@ const LandingFeatureSection = ({ onPrimaryAction }: LandingFeatureSectionProps) 
           <strong>주요 기능</strong>
         </h2>
         <p>
-          반려동물과 보호자의 삶을 더 편리하고
+          반려동물과 보호자에게 꼭 필요한
           <br />
-          풍요롭게 만들어주는 다양한 기능을 제공합니다.
+          주요 기능을 한곳에 모아 제공해드려요.
         </p>
         <button type="button" onClick={onPrimaryAction}>
           지금 시작하기
@@ -70,15 +91,11 @@ const LandingFeatureSection = ({ onPrimaryAction }: LandingFeatureSectionProps) 
       <div className="LandingFeatureSection_Grid">
         {featureCards.map((card) => (
           <article key={card.title} className={`LandingFeatureSection_Card ${card.tone}`}>
+            <div className="LandingFeatureSection_CardImageWrap">
+              <img src={card.image} alt={card.title} />
+            </div>
             <strong>{card.title}</strong>
             <p>{card.description}</p>
-            <div className="LandingFeatureSection_CardImages">
-              {card.images.map((image, index) => (
-                <div key={card.title + index} className="LandingFeatureSection_CardImageWrap">
-                  <img src={image} alt={card.title} />
-                </div>
-              ))}
-            </div>
           </article>
         ))}
       </div>
--- src/UserInfo/Login.tsx
+++ src/UserInfo/Login.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 import Kakaoimg from "../assets/images/btn_kakao.svg";
 import Naverimg from "../assets/images/btn_naver.svg";
 import Googleimg from "../assets/images/btn_google.svg";
 import Lodingimg from "../assets/images/login_loading.png";
 import PetBuddyLogo from "../assets/images/petbuddy_logo.svg";
 import LoginCollageImg from "../assets/images/petbuddy_login_collage.png";
-import { v4 as uuidv4 } from "uuid";
 import "./Login.scss";
 import UseInput from "../UseHook/UserInput";
 import { PUBGATEWAY_URL } from "../API/Api";
@@ -124,7 +124,7 @@ const Login: React.FC = () => {
   };
 
   const googleLogin = () => {
-    // TODO: 구글 OAuth 연결 시 이 함수에 인증 URL을 연결하면 됩니다.
+    // TODO: Google OAuth 연결 시 실제 인증 URL로 연결
   };
 
   return (
@@ -136,8 +136,8 @@ const Login: React.FC = () => {
         </div>
       )}
 
-      <section className="LoginShell">
-        <header className="LoginHeader">
+      <section className="Loginmain" aria-label="로그인">
+        <div className="LoginHeader">
           <button type="button" className="LoginBrand" onClick={() => navigate("/")}>
             <img src={PetBuddyLogo} alt="Aniverse" />
             <span>Aniverse</span>
@@ -145,122 +145,122 @@ const Login: React.FC = () => {
 
           <p>
             계정이 없으신가요?
-            <button type="button" onClick={() => navigate("/signs")}>회원가입</button>
+            <button type="button" onClick={() => navigate("/sign")}>회원가입</button>
           </p>
-        </header>
-
-        <div className="LoginContent">
-          <form className="LoginPanel" onSubmit={submitHandler}>
-            <div className="LoginTitle">
-              <h1>로그인</h1>
-              <p>
-                Aniverse에 오신 것을 환영합니다!
-                <br />
-                다양한 반려동물 보호자들과 함께 <strong>소통</strong>해보세요.
-              </p>
-            </div>
-
-            <div className="LoginField">
-              <label htmlFor="id">아이디</label>
-              <div className="LoginInputBox">
-                <span aria-hidden="true">⌾</span>
-                <input
-                  type="text"
-                  id="id"
-                  placeholder="아이디를 입력해주세요"
-                  value={EnterId}
-                  onChange={IdChangeHandler}
-                  onBlur={IdBlurHandler}
-                  autoComplete="username"
-                />
-              </div>
-            </div>
-
-            <div className="LoginField">
-              <label htmlFor="password">비밀번호</label>
-              <div className="LoginInputBox">
-                <span aria-hidden="true">⌕</span>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  placeholder="비밀번호를 입력해주세요"
-                  value={EnterPass}
-                  onChange={PassChangeHandler}
-                  onBlur={PassBlurHandler}
-                  autoComplete="current-password"
-                />
-                <button
-                  type="button"
-                  className="PasswordToggle"
-                  onClick={() => setShowPassword((prev) => !prev)}
-                >
-                  {showPassword ? "숨김" : "보기"}
-                </button>
-              </div>
-            </div>
-
-            <div className="LoginOptions">
-              <label>
-                <input type="checkbox" />
-                <span>로그인 상태 유지</span>
-              </label>
-              <Link to="/find">비밀번호 찾기</Link>
-            </div>
-
-            {isLoginFail && (
-              <p className="LoginError">회원정보가 일치하지 않습니다.</p>
-            )}
-
-            <button className="LoginSubmit" type="submit" disabled={!formIsValid || isLoading}>
-              로그인
-            </button>
-
-            <div className="LoginDivider">
-              <span />
-              <p>또는</p>
-              <span />
-            </div>
-
-            <div className="LoginSocial">
-              <p>간편하게 로그인하기</p>
-              <div>
-                <button type="button" onClick={kakaoLogin} aria-label="카카오 로그인">
-                  <img src={Kakaoimg} alt="" />
-                </button>
-                <button type="button" onClick={naverLogin} aria-label="네이버 로그인">
-                  <img src={Naverimg} alt="" />
-                </button>
-                <button type="button" onClick={googleLogin} aria-label="구글 로그인">
-                  <img src={Googleimg} alt="" />
-                </button>
-                <button type="button" aria-label="애플 로그인">
-                  <span></span>
-                </button>
-              </div>
-            </div>
-
-            <p className="LoginNotice">
-              로그인하면 Aniverse의 다양한 서비스를 이용할 수 있어요.
-            </p>
-          </form>
-
-          <aside className="LoginVisual" aria-label="Aniverse 소개 이미지">
-            <div className="LoginVisualText">
-              <span>♡</span>
-              <h2>
-                모든 반려동물과
-                <strong>모든 반려동물을 위한 플랫폼</strong>
-              </h2>
-              <p>
-                강아지, 고양이부터 파충류, 조류, 양서류, 곤충까지
-                <br />
-                모든 반려동물 보호자들이 모여 정보를 나누고 소통하는 공간입니다.
-              </p>
-            </div>
-            <img src={LoginCollageImg} alt="" />
-          </aside>
         </div>
+
+        <form className="LoginPanel" onSubmit={submitHandler}>
+          <div className="LoginTitle">
+            <h1>로그인</h1>
+            <p>
+              Aniverse에 오신 것을 환영합니다!
+              <br />
+              다양한 반려동물 보호자들과 함께 <strong>소통</strong>해보세요.
+            </p>
+          </div>
+
+          <div className="LoginField">
+            <label htmlFor="id">아이디</label>
+            <div className="LoginInputBox">
+              <span aria-hidden="true">◎</span>
+              <input
+                type="text"
+                id="id"
+                placeholder="아이디를 입력해주세요"
+                value={EnterId}
+                onChange={IdChangeHandler}
+                onBlur={IdBlurHandler}
+                autoComplete="username"
+              />
+            </div>
+          </div>
+
+          <div className="LoginField">
+            <label htmlFor="password">비밀번호</label>
+            <div className="LoginInputBox">
+              <span aria-hidden="true">◌</span>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="비밀번호를 입력해주세요"
+                value={EnterPass}
+                onChange={PassChangeHandler}
+                onBlur={PassBlurHandler}
+                autoComplete="current-password"
+              />
+              <button
+                type="button"
+                className="PasswordToggle"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? "숨김" : "보기"}
+              </button>
+            </div>
+          </div>
+
+          <div className="LoginOptions">
+            <label>
+              <input type="checkbox" />
+              <span>로그인 상태 유지</span>
+            </label>
+            <Link to="/find">비밀번호 찾기</Link>
+          </div>
+
+          {isLoginFail && (
+            <p className="LoginError">회원정보가 일치하지 않습니다.</p>
+          )}
+
+          <button className="LoginSubmit" type="submit" disabled={!formIsValid || isLoading}>
+            로그인
+          </button>
+
+          <div className="LoginDivider">
+            <span />
+            <p>또는</p>
+            <span />
+          </div>
+
+          <div className="LoginSocial">
+            <p>간편하게 로그인하기</p>
+            <div>
+              <button type="button" onClick={kakaoLogin} aria-label="카카오 로그인">
+                <img src={Kakaoimg} alt="" />
+              </button>
+              <button type="button" onClick={naverLogin} aria-label="네이버 로그인">
+                <img src={Naverimg} alt="" />
+              </button>
+              <button type="button" onClick={googleLogin} aria-label="구글 로그인">
+                <img src={Googleimg} alt="" />
+              </button>
+              <button type="button" aria-label="애플 로그인">
+                <span>X</span>
+              </button>
+            </div>
+          </div>
+
+          <p className="LoginNotice">
+            로그인하면 Aniverse의 다양한 서비스를 이용할 수 있어요.
+          </p>
+        </form>
       </section>
+
+      <aside className="LoginVisual" aria-label="Aniverse 소개 이미지">
+        <div className="LoginVisualContent">
+          <div className="LoginVisualText">
+            <span>♡</span>
+            <h2>
+              모든 반려동물과
+              <strong>모든 반려동물을 위한 플랫폼</strong>
+            </h2>
+            <p>
+              강아지, 고양이부터 파충류, 조류, 양서류, 곤충까지
+              <br />
+              모든 반려동물 보호자들이 모여 정보를 나누고 소통하는 공간입니다.
+            </p>
+          </div>
+          <img src={LoginCollageImg} alt="" />
+        </div>
+      </aside>
     </main>
   );
 };
